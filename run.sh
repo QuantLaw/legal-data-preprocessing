317aa9f0bad9da64570c3688335f7725716a93b8
Janis Beckedorf <janis@beckfish.de>
2020-05-06 18:02:09 +0200

    update sh

--- run.sh
+++ run.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
+python pipeline.py us all
 python pipeline.py de all
-python cd_pipeline.py de preprocess_graph cluster cluster_evolution_graph --snapshots all --pp-decay 0.75 --pp-merge 38000 --pp-ratio 20 --seed 1
-python cd_pipeline.py de preprocess_graph cluster cluster_evolution_graph --snapshots all --pp-merge 38000 --pp-ratio 20 --seed 1
