dda555a39a29e92fe3cbaeccce73a8bbef9a828b
Janis Beckedorf <janis@beckfish.de>
2020-10-28 16:14:02 +0100

    update snapshot mapping (fixes)

--- statutes_pipeline_steps/snapshot_mapping_edgelist.py
+++ statutes_pipeline_steps/snapshot_mapping_edgelist.py
@@ -1,4 +1,5 @@
 import json
+import multiprocessing
 import os
 import pickle
 from collections import Counter, deque
@@ -20,6 +21,8 @@ from utils.string_list_contains import StringContainsAlign
 
 
 class SnapshotMappingEdgelistStep(PipelineStep):
+    max_number_of_processes = min(max(multiprocessing.cpu_count() - 2, 1), 2)
+
     def __init__(
         self,
         source,
@@ -66,6 +69,9 @@ class SnapshotMappingEdgelistStep(PipelineStep):
     def execute_item(self, item):
         filename1, filename2 = item
 
+        dest_path = f"{self.destination}/{mapping_filename(item)}"
+        print(dest_path)
+
         data1 = self.load_pickle(filename1)
         data2 = self.load_pickle(filename2)
 
@@ -109,8 +115,9 @@ class SnapshotMappingEdgelistStep(PipelineStep):
         )
 
         print("Step 4")
-
-        with open(f"{self.destination}/{mapping_filename(item)}", "w") as f:
+        dest_path = f"{self.destination}/{mapping_filename(item)}"
+        print(dest_path)
+        with open(dest_path, "w") as f:
             json.dump(new_mappings, f)
 
         # only called to print stats
@@ -270,12 +277,19 @@ def map_text_containment(
     remaining_keys2,
     min_text_length=50,
 ):
+    remaining_keys1_list = sorted(remaining_keys1)
+    remaining_keys2_list = sorted(remaining_keys2)
+    leave_texts1_dict = {k: t for k, t in zip(data1["keys"], data1["texts"])}
+    leave_texts2_dict = {k: t for k, t in zip(data2["keys"], data2["texts"])}
+
     aligner = StringContainsAlign(min_text_length=min_text_length)
     aligner.text_list_0 = [
-        clip_text_for_containment_matching(t) for t in data1["texts"]
+        clip_text_for_containment_matching(leave_texts1_dict[k])
+        for k in remaining_keys1_list
     ]
     aligner.text_list_1 = [
-        clip_text_for_containment_matching(t) for t in data2["texts"]
+        clip_text_for_containment_matching(leave_texts2_dict[k])
+        for k in remaining_keys2_list
     ]
     aligner.create_index()
 
@@ -283,14 +297,11 @@ def map_text_containment(
     containment_idx_reversed = aligner.run(reversed=True)
     containment_idx_reversed = [(v, u) for u, v in containment_idx_reversed]
 
-    # Filter one to one matches
-    idx_1_counts = Counter()
-    idx_1_counts.update(u for u, v in containment_idx_forward)
-    idx_1_counts.update(u for u, v in containment_idx_reversed)
+    containment_idx = set(containment_idx_forward + containment_idx_reversed)
 
-    idx_2_counts = Counter()
-    idx_2_counts.update(v for u, v in containment_idx_forward)
-    idx_2_counts.update(v for u, v in containment_idx_reversed)
+    # Filter one to one matches
+    idx_1_counts = Counter(u for u, v in containment_idx)
+    idx_2_counts = Counter(v for u, v in containment_idx)
 
     unique_keys_1 = {idx for idx, cnt in idx_1_counts.items() if cnt == 1}
     unique_keys_2 = {idx for idx, cnt in idx_2_counts.items() if cnt == 1}
@@ -298,8 +309,8 @@ def map_text_containment(
     new_mappings = {}
     for u, v in containment_idx_forward + containment_idx_reversed:
         if u in unique_keys_1 and v in unique_keys_2:
-            u_key = data1["keys"][u]
-            v_key = data1["keys"][v]
+            u_key = remaining_keys1_list[u]
+            v_key = remaining_keys2_list[v]
             new_mappings[u_key] = v_key
 
     return new_mappings
